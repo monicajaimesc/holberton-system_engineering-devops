048a107f8679649ee31b3a25fccb39ee47084603
monicajaimesc <monikmaja@gmail.com>
2019-12-09 21:32:01 +0000

    requeriment fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,7 @@
-# Create a file in /tmp
-file {'/tmp/holberton':
- # Whether the file should exist, and if so what...
- ensure => file,
- path => '/tmp/holberton',
- mode => '0744',
- owner => 'www-data',
- group => 'www-data',
- # # The desired contents of a file, as a string...
- content => 'I love Puppet' 
-
+# Create a file called holberton in /tmp
+file { '/tmp/holberton':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
