6dc89d111b8f38a2214be037b6cbaa1f2892591d
monicajaimesc <monikmaja@gmail.com>
2020-02-19 12:21:52 -0500

    comment added

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# using strace to find out why Apache is returning a 500 e
+# using strace to find out why Apache is returning a 500 error
 exec { 'replace':
   command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
